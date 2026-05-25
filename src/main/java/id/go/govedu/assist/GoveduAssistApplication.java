98681f99091f747be64b6b7180b8a1bc53dd46c4
Irsyad Jamal Pratama Putra <mr.icadj@gmail.com>
2026-05-26 06:52:20 +0700

    Initial commit

---
+++ src/main/java/id/go/govedu/assist/GoveduAssistApplication.java
@@ -0,0 +1,13 @@
+package id.go.govedu.assist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoveduAssistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoveduAssistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/go/govedu/assist/GoveduAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package id.go.govedu.assist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoveduAssistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
